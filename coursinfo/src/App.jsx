f72e9aa437816fa993eae8abb8ca60f02dcfaf7c
anUnknownStoic <104714642+benbiji63@users.noreply.github.com>
2023-09-02 20:28:36 +0530

    Completed 1.1 exercise

---
+++ coursinfo/src/App.jsx
@@ -0,0 +1,112 @@
+import { useState } from 'react';
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// Initial stat of app before splitting
+// const App = () => {
+//   const course = 'Half Stack application development'
+//   const part1 = 'Fundamentals of React'
+//   const exercises1 = 10
+//   const part2 = 'Using props to pass data'
+//   const exercises2 = 7
+//   const part3 = 'State of a component'
+//   const exercises3 = 14
+
+//   return (
+//     <div>
+//       <h1>{course}</h1>
+//       <p>
+//         {part1} {exercises1}
+//       </p>
+//       <p>
+//         {part2} {exercises2}
+//       </p>
+//       <p>
+//         {part3} {exercises3}
+//       </p>
+//       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+//     </div>
+//   )
+// }
+
+const Header = props => {
+  return <h1>{props.course}</h1>;
+};
+
+const Footer = props => {
+  return (
+    <p>
+      Number of exercises {props.exercises.reduce((acc, num) => acc + num, 0)}
+    </p>
+  );
+};
+
+const Content = props => {
+  return (
+    <div>
+      <p>
+        {props.partDetails[0].part}
+        {props.partDetails[0].exercises}
+      </p>
+      <p>
+        {props.partDetails[1].part}
+        {props.partDetails[1].exercises}
+      </p>
+      <p>
+        {props.partDetails[2].part}
+        {props.partDetails[2].exercises}
+      </p>
+    </div>
+  );
+};
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        partDetails={[
+          { part: part1, exercises: exercises1 },
+          { part: part2, exercises: exercises2 },
+          { part: part3, exercises: exercises3 }
+        ]}
+      />
+      <Footer exercises={[exercises1, exercises2, exercises3]} />
+    </div>
+  );
+};
+
+export default App;

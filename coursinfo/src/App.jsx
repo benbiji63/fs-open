816baf5c36ed633d4a8cef5cbdd92195c0f6d222
anUnknownStoic <104714642+benbiji63@users.noreply.github.com>
2023-09-04 22:36:25 +0530

    Completed exercise 1.5

--- coursinfo/src/App.jsx
+++ coursinfo/src/App.jsx
@@ -86,17 +86,19 @@ const Footer = props => {
 };
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 },
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      { name: 'Fundamentals of React', exercises: 10 },
+      { name: 'Using props to pass data', exercises: 7 },
+      { name: 'State of a component', exercises: 14 },
+    ],
+  };
   return (
     <div>
-      <Header course={course} />
-      <Content partDetails={parts} />
-      <Footer exercises={parts.map(part=>part.exercises)} />
+      <Header course={course.name} />
+      <Content partDetails={course.parts} />
+      <Footer exercises={course.parts.map(part => part.exercises)} />
     </div>
   );
 };

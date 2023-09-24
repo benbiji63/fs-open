81f0be182030227de892a320bf70f7dd6eeb2a63
anUnknownStoic <104714642+benbiji63@users.noreply.github.com>
2023-09-24 08:46:38 +0530

    Completed exercise 2.1

--- coursinfo/src/components/course.jsx
+++ coursinfo/src/components/course.jsx
@@ -1,10 +1,11 @@
+// import { useState } from 'react';
 import Header1 from './header1.jsx'
 import Para from './para.jsx'
-import Data from './data.jsx'
 
 const Course = ({ course }) => (
+  <>
   <Header1 text={course.name}/>
-  // < Data
-)
-
-export default Course;
+  {course.parts.map(part=><Para text={`${part.name} ${part.exercises}`} key={part.id}/>)}
+  </>
+  )
+export default  Course;
